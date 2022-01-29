4dc762627dd93a4100adab4a97c3f36e202ac634
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2022-01-29 22:26:06 +0530

    Add files via upload

---
+++ steel_website/static/js/index.js
@@ -0,0 +1,41 @@
+let menuBtn = document.querySelector(".navbar .menu-btn");
+let menu = document.querySelector(".navbar .menu");
+let menuOpen = false;
+
+menuBtn.addEventListener('click', ()=>{
+    if(menuOpen===false){
+        menuBtn.querySelector('i').classList.replace("fa-bars","fa-times");
+        menu.style.left = "0";
+        menuOpen=true;
+    }
+    else
+    {
+        menuBtn.querySelector('i').classList.replace("fa-times","fa-bars");
+        menu.style.left = "-100%";
+        menuOpen=false;
+    }
+});
+
+
+// dropdown footer
+
+let designCard = document.querySelectorAll("footer .container .design");
+
+
+for(let i=0; i<designCard.length; i++){
+
+  let current = designCard[i].querySelector(".expand-btn");
+  let toggle = false;
+  let designItems = designCard[i].querySelectorAll(".items");
+  current.addEventListener('click',()=>{
+    if(!toggle){
+      designItems.forEach(item => item.style.display="block");
+      current.style.transform = "translate(-50%,10%) rotate(180deg)";
+    }
+    else{
+      designItems.forEach(item => item.style.display="none");
+      current.style.transform = "translate(-50%,10%) rotate(0deg)";
+    }
+    toggle=!toggle;
+  });
+}
