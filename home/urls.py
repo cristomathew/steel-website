5d6c68dc2291ffae704b637166782668d34b220c
cristomathew <cristomathew7@gmail.com>
2022-02-02 13:56:08 +0530

    final revisions

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('about/', views.about, name="about")
+    path('about/', views.about, name="about"),
+    path('services/', views.services, name="services"),
+    path('connect/', views.connect, name="connect"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def home(request):
 
 def about(request):
     return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def connect(request):
+    return render(request,'connect.html')
